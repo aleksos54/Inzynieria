daddc8aed866d70d923fa4fe1caa7fba978a5083
aleksos54 <aleks7777@poczta.fm>
2020-01-12 22:09:17 +0100

    Add files via upload

--- Ksiazka.h
+++ Ksiazka.h
@@ -1,14 +1,31 @@
-#pragma once
-#include "Autor.h"
+#ifndef KSIAZKA_H
+#define KSIAZKA_H
+#include <string>
 #include <iostream>
+#include <vector>
+#include "Autor.h"
+
 using namespace std;
 
-class Ksiazka:public Autor{
+class Autor;
+
+
+class Ksiazka{
 public:
 	string tytul;
 	void usun_egzemplarz();
 	Ksiazka zwroc_wolny_egzemplarz();
-	Ksiazka(string t) :Autor(a, b) ;
+	Ksiazka(string t);
+	vector<Autor>tab_autorow;
+	void przypisz_autora(Autor autor );
+	void usun_autora(Autor autor);
+	void wypisz_autorow_ksiazki();
+
+//	static int il_ksiazek;
+
+	
+	
 	~Ksiazka();
 };
 
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "KartaCzytelnika.h"
 #include "Czytelnik.h"
+#include "Autor.h"
+#include "Ksiazka.h"
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
 
 using namespace std;
 
@@ -10,6 +16,31 @@ void rejestruj();
 
 int main() {
 	
+	Autor a1("Adam","Mickiewicz");
+	Autor a2("Juliusz","Slowacki");
+	Autor a3("Chris","Brown");
+	Ksiazka k1("DZiady");
+	Ksiazka k2("Pan Tadeusz");
+	Ksiazka k3("Wallenrod");
+	Ksiazka k4("cyce");
+	a1.przypisz_ksiazke(k1);
+	a1.przypisz_ksiazke(k2);
+	a1.przypisz_ksiazke(k3);
+	a1.przypisz_ksiazke(k4);
+	a1.wypisz_ksiazke_danego_autora();
+	a1.usun_ksiazke(k2);
+	a1.wypisz_ksiazke_danego_autora();
+	cout<<endl;
+	cout<<endl;
+	k4.przypisz_autora(a1);
+	k4.przypisz_autora(a2);
+	k4.przypisz_autora(a3);
+	k4.wypisz_autorow_ksiazki();
+	k4.usun_autora(a3);
+	k4.wypisz_autorow_ksiazki();
+	
+	
+	/*
 	Czytelnik c1(1,"kamil", "kamil");
 	rejestruj();
 	return 0;
@@ -37,5 +68,6 @@ void rejestruj()
 	dane<<IDKarty<<" ";
 	
 	dane.close();
+	*/
 }
 
