c2de506300c1bb64063f552f26e2eec56873a83a
miksonos64 <59088391+miksonos64@users.noreply.github.com>
2020-01-12 22:31:29 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,75 @@
 #include <iostream>
 #include "KartaCzytelnika.h"
 #include "Czytelnik.h"
-#include "Autor.h"
-#include "Ksiazka.h"
-#include <vector>
-#include <algorithm>
-#include <fstream>
-
 
 using namespace std;
 
-
+int loguj();
 void rejestruj();
+void wypisz(string a, string b );
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+start:
+	cout<<"---Witaj w systemie bibliotecznym---"<<endl;
+	cout<<" 1. Zarejestruj sie "<<endl;
+	cout<<" 2. Zaloguj sie "<<endl;
+	int wybor;
+	cin>>wybor;
 	
-	Autor a1("Adam","Mickiewicz");
-	Autor a2("Juliusz","Slowacki");
-	Autor a3("Chris","Brown");
-	Ksiazka k1("DZiady");
-	Ksiazka k2("Pan Tadeusz");
-	Ksiazka k3("Wallenrod");
-	Ksiazka k4("cyce");
-	a1.przypisz_ksiazke(k1);
-	a1.przypisz_ksiazke(k2);
-	a1.przypisz_ksiazke(k3);
-	a1.przypisz_ksiazke(k4);
-	a1.wypisz_ksiazke_danego_autora();
-	a1.usun_ksiazke(k2);
-	a1.wypisz_ksiazke_danego_autora();
-	cout<<endl;
-	cout<<endl;
-	k4.przypisz_autora(a1);
-	k4.przypisz_autora(a2);
-	k4.przypisz_autora(a3);
-	k4.wypisz_autorow_ksiazki();
-	k4.usun_autora(a3);
-	k4.wypisz_autorow_ksiazki();
+	switch(wybor)
+	{
+		case 1:
+			rejestruj();
+			cout<<endl;
+			goto start;
+			break;
+		case 2:
+			loguj();
+			goto inter1;
+			break;
+		case 0:
+			//admin();
+			goto interad;
+			break;
+	}
 	
+inter1:
 	
-	/*
+	cout<<"---Panel czytelnika---"<<endl;
+	cout<<" 1. Wyszukaj ksiazke "<<endl;
+	cout<<" 2. Wypozycz ksiazke "<<endl;
+	cout<<" 3. Zarezerwuj ksiazke "<<endl;
+	//cout<<" 3. Oddaj ksiazke "<<endl;
+	
+	int wybor1;
+	cin>>wybor1;
+	
+	switch(wybor1)
+	{
+		case 1:
+			//wyszukaj();
+			cout<<"elo";
+			cout<<endl;
+			break;
+		case 2:
+				cout<<"elo2";
+			//wypozycz();
+			break;
+		case 0:
+			//admin();
+			break;
+	}
+	
+interad:
+	cout<<"---Panel bibliotekarza---"<<endl;
+	cout<<" 1. Dodaj ksiazke "<<endl;
+	cout<<" 2. ";
+	
+	koniec:
+		
+	
 	Czytelnik c1(1,"kamil", "kamil");
-	rejestruj();
 	return 0;
 }
 void rejestruj()
@@ -50,13 +77,14 @@ void rejestruj()
 	fstream dane;
 	string imie;
     string nazwisko;
-	int IDKarty;
+    string haslo;
+	string IDKarty;
 
-	cout<<"Podaj imie:";		
-	cin>>imie;	
+	cout<<"Podaj imie:";		cin>>imie;	
 	cout<<"Podaj nazwisko:";	cin>>nazwisko;	
 	cout<<"Podaj IDKarty:";		cin>>IDKarty;
-	dane.open("data.txt", ios::out | ios::app);
+	cout<<"Podaj haslo";		cin>>haslo;
+	dane.open("data.txt", ios::app);
    //	dane.open("dane.txt",ios::out | ios::app);
 
 
@@ -67,7 +95,57 @@ void rejestruj()
 			
 	dane<<IDKarty<<" ";
 	
+	dane<<haslo<<" \n";
+	
 	dane.close();
-	*/
 }
+
+int loguj()
+ {
+	fstream plik;
+	string line1;
+	string line2;
+	string line3;
+	string line4;
+	string IDKarty1;
+	string haslo;
+	cout<<"Podaj login ";	cin>>IDKarty1;
+	cout<<"Podaj haslo ";	cin>>haslo;
+	cout<<endl;
+	plik.open("data.txt", ios::in | ios::out);
+	if (plik.good()==true)
+	{
+		while(!plik.eof())
+		{
+			plik>>line1;
+			plik>>line2;
+			plik>>line3;
+			plik>>line4;
+		if(IDKarty1==line3 && haslo==line4){
+
+			//cout<<"witaj "<< line1 <<" "<< line2<<endl;
+			wypisz(line1, line2);
+			return 1;
+			//goto a;
+		}
+		else{
+		
+		//cout<<"blad";
+		
+		
+			}
+		}
+		//a:
+	plik.close();
+	cout<<"nie ma takiego id lub hasla"<<endl<<endl;
+	return 1;
+	}
+}
+
+void wypisz(string a, string b ){
+	cout<<"Witaj "<<a<<" "<<b<<endl<<endl;
+}
+
+
+
 
